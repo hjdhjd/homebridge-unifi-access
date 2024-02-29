408f652a3dd83e00052eac087612de7255f03b6f
Patric Wilms <patric.wilms@rtl.de>
2024-02-29 22:27:10 +0100

    fix: typo platform alias to UniFi

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = "Unifi Access";
+export const PLATFORM_NAME = "UniFi Access";
 
 /**
  * This must match the name of your plugin as defined the package.json
